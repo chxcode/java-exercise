e0058e0b653c34ba0ae0edb32cef2d825e0663f0
changxuan <chang-xuan@qq.com>
2020-04-15 15:16:00 +0800

    新增：源码分析

---
+++ src/resourcecode/CodeTest.java
@@ -0,0 +1,13 @@
+package resourcecode;
+
+/**
+ * @Author: ChangXuan
+ * @Decription: 源码分析
+ * @Date: 12:05 2020/4/6
+ **/
+public class CodeTest {
+    public static void main(String[] args) {
+        String s = "abc";
+        System.out.println(s.hashCode());
+    }
+}
