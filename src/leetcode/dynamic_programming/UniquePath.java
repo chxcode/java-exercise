f19e20e86925f7318f91f76c684694b9d2d8013c
changxuan <yanghuo_cx@163.com>
2020-06-30 21:26:06 +0800

    新增：动态规划题目

---
+++ src/leetcode/dynamic_programming/UniquePath.java
@@ -0,0 +1,27 @@
+package leetcode.dynamic_programming;
+
+/**
+ * @ClassName UniquePath
+ * @Description A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+ * The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+ * How many possible unique paths are there?
+ * @Author changxuan
+ * @Date 2020/6/29 下午8:25
+ **/
+public class UniquePath {
+    public static void main(String[] args) {
+        System.out.println(uniquePaths(100,100));
+    }
+
+    public static int uniquePaths(int m, int n) {
+        int[] temp = new int[m];
+        for (int i = 0; i < m; i++)
+            temp[i] = 1;
+        for (int j = 0; j < n-1; j++){
+            for (int c = 1; c < m; c++){
+                temp[c] = temp[c-1] + temp[c];
+            }
+        }
+        return temp[m-1];
+    }
+}
