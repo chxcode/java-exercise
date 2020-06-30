6fe3ebd58937c4a75ad98bdc60006f750bf0e21d
changxuan <yanghuo_cx@163.com>
2020-06-30 22:20:38 +0800

    新增：动态规划题目

---
+++ src/leetcode/dynamic_programming/UniqueBinarySearchTrees.java
@@ -0,0 +1,18 @@
+package leetcode.dynamic_programming;
+
+/**
+ * @ClassName UniqueBinarySearchTrees
+ * @Description Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?
+ * @Author changxuan
+ * @Date 2020/6/30 下午10:02
+ **/
+public class UniqueBinarySearchTrees {
+    public static void main(String[] args) {
+
+    }
+
+    public int numTrees(int n){
+
+        return 0;
+    }
+}
