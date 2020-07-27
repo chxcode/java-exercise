27d6920cd60feef05c3843786a6f0ab33e73b18a
changxuan <yanghuo_cx@163.com>
2020-07-27 23:37:20 +0800

    add：dp

---
+++ src/leetcode/dynamic_programming/CoinLcci.java
@@ -0,0 +1,26 @@
+package leetcode.dynamic_programming;
+
+/**
+ * @ClassName CoinLcci
+ * @Description 硬币
+ * @Author changxuan
+ * @Date 2020/7/26 下午11:04
+ **/
+public class CoinLcci {
+
+    public static void main(String[] args) {
+        System.out.println(waysToChange(5));
+    }
+
+    public static int waysToChange(int n){
+        int[] dp = new int[n+1];
+        int[] coins = {1,5,10,25};
+        dp[0] = 1;
+        for (int coin : coins){
+            for (int i = coin; i <= n; i++ ){
+                dp[i] = (dp[i]+dp[i-coin]) % 1000000007;
+            }
+        }
+        return dp[n];
+    }
+}
