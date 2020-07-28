139fbcaa833c1f5ab0c19cda96208b73cbe57a09
changxuan <yanghuo_cx@163.com>
2020-07-28 21:10:39 +0800

    add：前缀和

---
+++ src/leetcode/dynamic_programming/ContinuousSubarraySum.java
@@ -0,0 +1,35 @@
+package leetcode.dynamic_programming;
+
+import java.util.Arrays;
+
+/**
+ * @ClassName ContinuousSubarraySum
+ * @Description 前缀和
+ * @Author changxuan
+ * @Date 2020/7/27 下午11:54
+ **/
+public class ContinuousSubarraySum {
+    public static void main(String[] args) {
+        int[] arr = {1,1,1,1,1,1,1,1};
+        System.out.println(checkSubarraySum(arr, 3));
+    }
+    public static boolean checkSubarraySum(int[] nums, int k) {
+        int len = nums.length;
+        if (len == 0)
+            return false;
+        int[] preSum = new int[len+1];
+        for (int i = 1; i < len+1; i++){
+            preSum[i] = preSum[i-1] + nums[i-1];
+        }
+        System.out.println(Arrays.toString(preSum));
+        for (int i = len; i > 1; i--){
+            for (int j = 0; j < i-1; j++){
+                if ((k != 0 && (preSum[i] - preSum[j]) % k == 0) ||
+                (k == 0 && preSum[i] - preSum[j] == 0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
