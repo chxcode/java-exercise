0d8031dbcec8b820f88d98f18360ff7a2a33d05f
changxuan <yanghuo_cx@163.com>
2020-07-29 23:46:14 +0800

    add：test

---
+++ src/strange/IntegerTest.java
@@ -0,0 +1,19 @@
+package strange;
+
+/**
+ * @ClassName IntergetTest
+ * @Description
+ * @Author changxuan
+ * @Date 2020/7/29 下午11:41
+ **/
+public class IntegerTest {
+    public static void main(String[] args) {
+        Integer one = 1;
+        test(one);
+        //System.out.println(one == 1);
+    }
+
+    public static void test(Integer one){
+        System.out.println(one == 1);
+    }
+}
