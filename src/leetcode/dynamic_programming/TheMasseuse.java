d0dea4fbc85efc2f70733b24a97f368dca1cec3d
changxuan <yanghuo_cx@163.com>
2020-07-31 22:52:45 +0800

    add：按摩师

---
+++ src/leetcode/dynamic_programming/TheMasseuse.java
@@ -0,0 +1,31 @@
+package leetcode.dynamic_programming;
+
+/**
+ * @ClassName TheMasseuse
+ * @Description 按摩师
+ * @Author changxuan
+ * @Date 2020/7/31 下午10:29
+ **/
+public class TheMasseuse {
+    public static void main(String[] args) {
+        int[] arrays = {1,2,3,1};
+        System.out.println(massage(arrays));
+
+        int[] arraysTwo = {2,7,9,3,1};
+        System.out.println(massage(arraysTwo));
+
+        int[] arraysThree = {2,1,4,5,3,1,1,3};
+        System.out.println(massage(arraysThree));
+    }
+    public static int massage(int[] nums) {
+        if (nums.length == 0){
+            return 0;
+        }
+        int[] dp = new int[nums.length+1];
+        dp[1] = nums[0];
+        for (int i = 2; i < dp.length; i++){
+            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i-1]);
+        }
+        return dp[nums.length];
+    }
+}
