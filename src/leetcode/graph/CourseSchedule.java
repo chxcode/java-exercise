d5610f2e6321fcfaa7c7da4efe203d681e9b3ed1
changxuan <yanghuo_cx@163.com>
2020-08-05 22:26:47 +0800

    add: 课程表

--- src/leetcode/graph/CourseSchedule.java
+++ src/leetcode/graph/CourseSchedule.java
@@ -1,6 +1,7 @@
 package leetcode.graph;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -15,34 +16,45 @@ public class CourseSchedule {
         int[][] prerequisites = {{1,0}};
         int[][] test = {{1,0},{0,1}};
         int[][] test2 = {{1,0},{0,2},{2,1}};
+        int[][] test4 = {{2,0},{1,0},{3,1},{3,2},{1,3}};
+        int[][] test5 = {{1,0}};
+        int[][] test6 = {{0,1},{0,2},{1,2}};
 //        System.out.println(canFinish(2, prerequisites));
 //        System.out.println(canFinish(2, test));
-        System.out.println(canFinish(3, test2));
+        System.out.println(canFinish(4, test4));
     }
 
     public static boolean canFinish(int numCourses, int[][] prerequisites) {
         int[][] graph = new int[numCourses][numCourses];
         int[] flag = new int[numCourses];
+        boolean result;
         // 填充边
         for (int i = 0; i < prerequisites.length; i++){
             graph[prerequisites[i][0]][prerequisites[i][1]] = 1;
         }
         for (int i = 0; i < numCourses-1; i++){
             if (flag[i] == 0){
-                 return dfs(graph, flag, i);
+                 result = dfs(graph, flag, i);
+                 if (!result)
+                     return false;
             }
+            Arrays.fill(flag, 0);
         }
         return true;
     }
     public static boolean dfs(int[][] graph, int[] flag, int node){
         if (flag[node] == 1)
             return false;
+        if (flag[node] == -1)
+            return true;
         flag[node] = 1;
         for (int i = 0; i < graph[0].length; i++){
             if (graph[node][i] != 0){
-                return dfs(graph, flag, i);
+                if (!dfs(graph, flag, i))
+                    return false;
             }
         }
+        flag[node] = -1;
         return true;
     }
     public boolean canFinishRight(int numCourses, int[][] prerequisites) {
