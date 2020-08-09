1dc2660ee55f4e268422b080e754d70a3f61c5ca
changxuan <yanghuo_cx@163.com>
2020-08-09 22:22:05 +0800

    update: 2的幂

---
+++ src/leetcode/bitwise/PowerOfTwo.java
@@ -0,0 +1,32 @@
+package leetcode.bitwise;
+
+/**
+ * @ClassName PowerOfTwo
+ * @Description 2的幂
+ * @Author changxuan
+ * @Date 2020/8/9 下午10:00
+ **/
+public class PowerOfTwo {
+    public static void main(String[] args) {
+        //System.out.println(1>>1);
+        //System.out.println(isPowerOfTwo(7));
+        System.out.println(8 & 7);
+    }
+    public boolean isPowerOfTwos(int n) {
+        if (n <= 0) {
+            return false;
+        }
+
+        return (n & (n - 1)) == 0;
+    }
+    public static boolean isPowerOfTwo(int n) {
+        if (n == 0) return  false;
+        if (n == 1) return true;
+        while (n != 1){
+            if (n%2 != 0)
+                return false;
+            n = n >>1;
+        }
+        return true;
+    }
+}
