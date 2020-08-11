8118de896254d714fdf3f99819409f1d0bfbd333
changxuan <yanghuo_cx@163.com>
2020-08-11 19:54:07 +0800

    add: 数组中的第K个最大元素

---
+++ src/leetcode/array/KthLargestElement.java
@@ -0,0 +1,26 @@
+package leetcode.array;
+
+/**
+ * @ClassName KthLargestElement
+ * @Description 数组中的第K个最大元素
+ * @Author changxuan
+ * @Date 2020/8/11 下午7:33
+ **/
+public class KthLargestElement {
+
+    public int findKthLargest(int[] nums, int k) {
+        for (int i = 0; i < nums.length; i++){
+            boolean flag = false;
+            for (int j = 1; j < nums.length - i; j++){
+                if (nums[j-1] > nums[j]){
+                    int temp = nums[j-1];
+                    nums[j-1] = nums[j];
+                    nums[j] = temp;
+                    flag = true;
+                }
+            }
+            if ((i + 1) == k || !flag) return nums[nums.length-k];
+        }
+        return nums[0];
+    }
+}
