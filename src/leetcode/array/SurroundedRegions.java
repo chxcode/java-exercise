421fd9f16fd13dbeef646a3256a3ffe31189af1b
changxuan <yanghuo_cx@163.com>
2020-08-11 20:56:33 +0800

    add: 被围绕的区域

---
+++ src/leetcode/array/SurroundedRegions.java
@@ -0,0 +1,51 @@
+package leetcode.array;
+
+/**
+ * @ClassName SurroundedRegions
+ * @Description 被围绕的区域
+ * @Author changxuan
+ * @Date 2020/8/11 下午8:32
+ **/
+public class SurroundedRegions {
+    public static void main(String[] args) {
+    }
+
+    // 换个思路， 从边开始入手 如是O则向里搜索 都改成其他的字符，最后再遍历一次将 其他字符变为O，O变为X
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0) return;
+        int m = board.length;
+        int n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // 从边缘o开始搜索
+                boolean isEdge = i == 0 || j == 0 || i == m - 1 || j == n - 1;
+                if (isEdge && board[i][j] == 'O') {
+                    dfs(board, i, j);
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+                if (board[i][j] == '#') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    public void dfs(char[][] board, int i, int j) {
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] == 'X' || board[i][j] == '#') {
+            // board[i][j] == '#' 说明已经搜索过了.
+            return;
+        }
+        board[i][j] = '#';
+        dfs(board, i - 1, j); // 上
+        dfs(board, i + 1, j); // 下
+        dfs(board, i, j - 1); // 左
+        dfs(board, i, j + 1); // 右
+    }
+}
