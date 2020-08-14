95f83255fb44c05d0f15d0892d27c8fa854f0573
changxuan <yanghuo_cx@163.com>
2020-08-14 21:43:44 +0800

    add: 有效括号

---
+++ src/array/ValidParentheses.java
@@ -0,0 +1,71 @@
+package array;
+
+
+import java.util.Stack;
+
+/**
+ * @ClassName ValidParentheses
+ * @Description 有效括号
+ * @Author changxuan
+ * @Date 2020/8/14 下午9:12
+ **/
+public class ValidParentheses {
+
+    private Stack<Character> stack = new Stack<Character>();
+
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0)
+            return false;
+        char[] sArr = s.toCharArray();
+        int i = 0, j = s.length();
+        while (i < j){
+            switch (sArr[i]){
+                case '(':
+                case '[':
+                case '{':
+                    stack.push(sArr[i]);
+                    break;
+                default:
+                    if (stack.peek().equals(sArr[i]))
+                        stack.pop();
+                    else
+                        return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isValidPro(String s) {
+        if (s.length() % 2 != 0)
+            return false;
+        char[] sArr = s.toCharArray();
+        char[] temp = new char[sArr.length];
+        int j = 0;
+        for (int i = 0; i < sArr.length; i++){
+            switch (sArr[i]) {
+                case '(':
+                case '[':
+                case '{':
+                    temp[j] = sArr[i];
+                    j++;
+                    break;
+                default:
+                    if (j != 0 && judge(temp[j-1], sArr[i]))
+                        j--;
+                    else
+                        return false;
+            }
+        }
+        return j == 0 ? true : false;
+    }
+
+    public boolean judge(char right, char left){
+        if (right == '(' && left == ')')
+            return true;
+        if (right == '[' && left == ']')
+            return true;
+        if (right == '{' && left == '}')
+            return true;
+        return false;
+    }
+}
