5fb193b350ff6a946635ee83367ede2851e99efb
changxuan <yanghuo_cx@163.com>
2020-08-14 21:56:07 +0800

    add: 只出现一次的数字

---
+++ src/leetcode/bitwise/SingleNumber.java
@@ -0,0 +1,22 @@
+package leetcode.bitwise;
+
+/**
+ * @ClassName SingleNumber
+ * @Description 只出现一次的数字
+ * @Author changxuan
+ * @Date 2020/8/14 下午9:49
+ **/
+public class SingleNumber {
+    public static void main(String[] args) {
+        System.out.println(4 ^ 1 ^ 2 ^ 1 ^ 2 ^ 4 ^ 0);
+        System.out.println(2 ^ 2 ^ 1);
+        int[] ints = {4,1,2,1,2};
+        System.out.println(singleNumber(ints));
+    }
+    public static int singleNumber(int[] nums) {
+        int result = nums[0];
+        for (int i = 1; i < nums.length; i++)
+            result ^= nums[i];
+        return result;
+    }
+}
