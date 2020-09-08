eae1146596290547bbd1a4cb4a979dad8175bb60
changxuan <yanghuo_cx@163.com>
2020-09-08 22:33:01 +0800

    add：link

--- src/leetcode/recursive/ComputeOneToN.java
+++ src/leetcode/recursive/ComputeOneToN.java
@@ -2,7 +2,7 @@ package leetcode.recursive;
 
 /**
  * @ClassName ComputeOneToN
- * @Description 求1+2+…+n
+ * @Description 求1+2+…+n https://leetcode-cn.com/problems/qiu-12n-lcof
  * @Author changxuan
  * @Date 2020/8/30 下午10:52
  **/
