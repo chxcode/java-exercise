4f45e1239680d790bec7f5faa2649e0626db468a
chxcode <yanghuo_cx@163.com>
2020-09-13 11:45:20 +0800

    add:link

--- src/leetcode/array/RemoveElement.java
+++ src/leetcode/array/RemoveElement.java
@@ -2,16 +2,16 @@ package leetcode.array;
 
 /**
  * @Author: ChangXuan
- * @Decription: 移除元素
+ * @Decription: 移除元素 https://leetcode-cn.com/problems/remove-element/
  * @Date: 22:39 2020/8/22
  **/
 public class RemoveElement {
 
     /**
      * 快慢指针
-     * @param nums
-     * @param val
-     * @return
+     * @param nums 数组
+     * @param val 被移除元素
+     * @return 新数组长度
      */
     public int removeElement(int[] nums, int val) {
         if(nums == null || nums.length == 0) return 0;
