035921afc0c62db7e5f54b8e97616c590384b4ae
chxcode <yanghuo_cx@163.com>
2020-09-13 16:48:51 +0800

    add：link

--- src/leetcode/dynamic_programming/UniqueBinarySearchTrees.java
+++ src/leetcode/dynamic_programming/UniqueBinarySearchTrees.java
@@ -3,6 +3,7 @@ package leetcode.dynamic_programming;
 /**
  * @ClassName UniqueBinarySearchTrees
  * @Description Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?
+ * https://leetcode-cn.com/problems/unique-binary-search-trees/
  * @Author changxuan
  * @Date 2020/6/30 下午10:02
  **/
--- src/leetcode/dynamic_programming/UniquePath.java
+++ src/leetcode/dynamic_programming/UniquePath.java
@@ -1,5 +1,7 @@
 package leetcode.dynamic_programming;
 
+import java.util.Arrays;
+
 /**
  * @ClassName UniquePath
  * @Description A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
@@ -15,8 +17,7 @@ public class UniquePath {
 
     public static int uniquePaths(int m, int n) {
         int[] temp = new int[m];
-        for (int i = 0; i < m; i++)
-            temp[i] = 1;
+        Arrays.fill(temp,1);
         for (int j = 0; j < n-1; j++){
             for (int c = 1; c < m; c++){
                 temp[c] = temp[c-1] + temp[c];
