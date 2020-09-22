796df38317dc848ff06c8d77c13abce712c814b1
changxuan <yanghuo_cx@163.com>
2020-09-22 22:00:26 +0800

    add: String Test

--- src/strange/StringTest.java
+++ src/strange/StringTest.java
@@ -1,5 +1,7 @@
 package strange;
 
+import java.nio.charset.Charset;
+
 /**
  * @Author: ChangXuan
  * @Decription:
@@ -7,7 +9,15 @@ package strange;
  **/
 public class StringTest {
     public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("abc");
+        String t = null;
+        builder.insert(3, t);
+        System.out.println(builder.toString());
+        System.out.println(8>>2);
         String str = "app/sss/a";
+        System.out.println(str.getBytes()[0]);
+        System.out.println(Charset.defaultCharset().name());
         System.out.println(str.replaceFirst("/","").replaceAll("/","-"));
     }
 }
