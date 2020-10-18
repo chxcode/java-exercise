ba5fa8bbdab2cfc1a1997777abbb80c788462c28
chxcode <yanghuo_cx@163.com>
2020-10-18 15:50:38 +0800

    update:反转链表

--- src/leetcode/list/ReverseLinkedList.java
+++ src/leetcode/list/ReverseLinkedList.java
@@ -2,11 +2,29 @@ package leetcode.list;
 
 /**
  * @ClassName ReverseLinkedList
- * @Description 反转链表
+ * @Description 反转链表  https://leetcode-cn.com/problems/reverse-linked-list
  * @Author changxuan
  * @Date 2020/8/11 下午7:57
  **/
 public class ReverseLinkedList {
+
+    public ListNode reverseListPro(ListNode head) {
+        //申请节点，pre和 cur，pre指向null
+        ListNode pre = null;
+        ListNode cur = head;
+        ListNode tmp = null;
+        while(cur!=null) {
+            //记录当前节点的下一个节点
+            tmp = cur.next;
+            //然后将当前节点指向pre
+            cur.next = pre;
+            //pre和cur节点都前进一位
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+
     public ListNode reverseList(ListNode head) {
         ListNode curr, pre = null;
         while (head.next != null){
