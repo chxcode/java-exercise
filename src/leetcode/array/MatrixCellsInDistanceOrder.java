bfbeb3ae0729ca5b19dc74e4366c71ff353ee46e
changxuan <yanghuo_cx@163.com>
2020-11-17 22:17:44 +0800

    add:距离顺序排列矩阵单元格

---
+++ src/leetcode/array/MatrixCellsInDistanceOrder.java
@@ -0,0 +1,33 @@
+package leetcode.array;
+
+import java.util.Arrays;
+
+/**
+ * @ClassName MatrixCellsInDistanceOrder
+ * @Description 距离顺序排列矩阵单元格 https://leetcode-cn.com/problems/matrix-cells-in-distance-order/
+ * @Author changxuan
+ * @Date 2020/11/17 下午10:15
+ **/
+public class MatrixCellsInDistanceOrder {
+    public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
+        int[][] re = new int[R * C][2];
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                int t = i*C+j;
+                re[t][0] = i;
+                re[t][1] = j;
+            }
+        }
+        Arrays.sort(re, (arr1, arr2) -> {
+            int d1 = dist(arr1[0], arr1[1], r0, c0);
+            int d2 = dist(arr2[0], arr2[1], r0, c0);
+            return Integer.compare(d1, d2);
+        });
+
+        return re;
+    }
+
+    private int dist(int r1,int c1,int r2,int c2) {
+        return Math.abs(r1 - r2) + Math.abs(c1 - c2);
+    }
+}
