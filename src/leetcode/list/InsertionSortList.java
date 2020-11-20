66afd939fac440dfb5c869c4809aa29b4e703b57
changxuan <yanghuo_cx@163.com>
2020-11-20 21:49:41 +0800

    add:链表插入排序

---
+++ src/leetcode/list/InsertionSortList.java
@@ -0,0 +1,41 @@
+package leetcode.list;
+
+/**
+ * @ClassName InsertionSortList
+ * @Description
+ * @Author changxuan
+ * @Date 2020/11/20 下午9:42
+ **/
+public class InsertionSortList {
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode lastSorted = head, curr = head.next;
+        while (curr != null) {
+            if (lastSorted.val <= curr.val) {
+                lastSorted = lastSorted.next;
+            } else {
+                ListNode prev = dummyHead;
+                while (prev.next.val <= curr.val) {
+                    prev = prev.next;
+                }
+                lastSorted.next = curr.next;
+                curr.next = prev.next;
+                prev.next = curr;
+            }
+            curr = lastSorted.next;
+        }
+        return dummyHead.next;
+    }
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
