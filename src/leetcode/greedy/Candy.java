2d92e4e5a6ee01758d16702e01950d1715a65136
changxuan <yanghuo_cx@163.com>
2020-12-24 21:00:43 +0800

    add:分发糖果

---
+++ src/leetcode/greedy/Candy.java
@@ -0,0 +1,34 @@
+package leetcode.greedy;
+
+import java.util.Arrays;
+
+/**
+ * @ClassName Candy
+ * @Description 分发糖果
+ * https://leetcode-cn.com/problems/candy/
+ * @Author changxuan
+ * @Date 2020/12/24 下午8:36
+ **/
+public class Candy {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] left = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && ratings[i] > ratings[i - 1]) {
+                left[i] = left[i - 1] + 1;
+            } else {
+                left[i] = 1;
+            }
+        }
+        int right = 0, ret = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (i < n - 1 && ratings[i] > ratings[i + 1]) {
+                right++;
+            } else {
+                right = 1;
+            }
+            ret += Math.max(left[i], right);
+        }
+        return ret;
+    }
+}
