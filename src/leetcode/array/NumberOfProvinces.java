33e659dadd747089a36ffe6698dc61e2d346311c
changxuan <yanghuo_cx@163.com>
2021-01-07 23:02:26 +0800

    add: 省份数量

---
+++ src/leetcode/array/NumberOfProvinces.java
@@ -0,0 +1,31 @@
+package leetcode.array;
+
+/**
+ * @ClassName NumberOfProvinces
+ * @Description 省份数量 https://leetcode-cn.com/problems/number-of-provinces/
+ * @Author changxuan
+ * @Date 2021/1/7 下午10:55
+ **/
+public class NumberOfProvinces {
+    public int findCircleNum(int[][] isConnected) {
+        int provinces = isConnected.length;
+        boolean[] visited = new boolean[provinces];
+        int circles = 0;
+        for (int i = 0; i < provinces; i++) {
+            if (!visited[i]) {
+                dfs(isConnected, visited, provinces, i);
+                circles++;
+            }
+        }
+        return circles;
+    }
+
+    public void dfs(int[][] isConnected, boolean[] visited, int provinces, int i) {
+        for (int j = 0; j < provinces; j++) {
+            if (isConnected[i][j] == 1 && !visited[j]) {
+                visited[j] = true;
+                dfs(isConnected, visited, provinces, j);
+            }
+        }
+    }
+}
