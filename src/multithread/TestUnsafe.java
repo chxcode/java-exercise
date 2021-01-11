e96f778abf933acb528c1affce46a452f1c9feed
changxuan <yanghuo_cx@163.com>
2021-01-11 22:19:47 +0800

    add:Unsafe

---
+++ src/multithread/TestUnsafe.java
@@ -0,0 +1,36 @@
+package multithread;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+/**
+ * @ClassName TestUnsafe
+ * @Description Unsafe
+ * @Author changxuan
+ * @Date 2021/1/11 下午10:07
+ **/
+public class TestUnsafe {
+    static Unsafe unsafe;
+    static long stateOffset;
+
+    private volatile long state = 0;
+
+    static {
+        try {
+            Field field = Unsafe.class.getDeclaredField("theUnsafe");
+            field.setAccessible(true);
+            unsafe = (Unsafe) field.get(null);
+            stateOffset = unsafe.objectFieldOffset(
+                    TestUnsafe.class.getDeclaredField("state"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        TestUnsafe testUnsafe = new TestUnsafe();
+        Boolean success = unsafe.compareAndSwapInt(testUnsafe, stateOffset, 0, 1);
+        System.out.println(success);
+    }
+}
